226d1e18ae1c7a2869ce378181bf6176e6adc583
Vladimir Rak <made.in.cccp85@gmail.com>
2020-04-19 01:10:26 +0600

    feature(components): add btn/btn.toolbar/btn.group

---
+++ src/Btn/Toolbar.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LaravelCMC\Components\Btn;
+
+use LaravelCMC\Components\BootstrapComponent;
+use LaravelCMC\Components\Utilities\Flex;
+
+class Toolbar extends BootstrapComponent
+{
+    use Flex;
+
+    /**
+     * @var string
+     */
+    protected $bootstrapClass = 'btn-toolbar';
+
+    /**
+     * Alert constructor.
+     * @param string $tag
+     * @param null|string $theme
+     * @param null|string $content
+     * @param null|string $justify
+     */
+    public function __construct($tag = null, $content = null, $justify = null) {
+        parent::__construct($tag, $content);
+
+        $this->element()->setAttribute('role', 'toolbar');
+
+        $this->element()->addClass($this->justifyClass($justify));
+    }
+}
